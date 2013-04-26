4cd760d2e7a4db64e8dbe039d383d5cec8fa4176
Elio <elio@elio.de>
2013-04-26 16:29:00 +0200

    Headers

--- Source/dxConnector.cpp
+++ Source/dxConnector.cpp
@@ -1,3 +1,38 @@
+/**
+	Copyright 2013 Elio <elio@r-revue.de>
+	
+	
+	This file is part of RR-DXBridge
+
+    RR-DXBridge is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    RR-DXBridge is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with RR-DXBridge.  If not, see <http://www.gnu.org/licenses/>.
+
+    Diese Datei ist Teil von RR-DXBridge.
+
+    RR-DXBridge ist Freie Software: Sie können es unter den Bedingungen
+    der GNU Lesser General Public License, wie von der Free Software Foundation,
+    Version 3 der Lizenz oder (nach Ihrer Option) jeder späteren
+    veröffentlichten Version, weiterverbreiten und/oder modifizieren.
+
+    RR-DXBridge wird in der Hoffnung, dass es nützlich sein wird, aber
+    OHNE JEDE GEWÄHELEISTUNG, bereitgestellt; sogar ohne die implizite
+    Gewährleistung der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN ZWECK.
+    Siehe die GNU Lesser General Public License für weitere Details.
+
+    Sie sollten eine Kopie der GNU Lesser General Public License zusammen mit diesem
+    Programm erhalten haben. Wenn nicht, siehe <http://www.gnu.org/licenses/>.
+ */
+
 #include "dxconnector.h"
 #include <string>
 
